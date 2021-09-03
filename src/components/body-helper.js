42dca078b88d35d54c34632d1bd2ce1d121d2287
Issei0819 <issei.kurata819@gmail.com>
2021-09-04 07:30:44 +0900

    2021-09-04-ver1

--- src/components/body-helper.js
+++ src/components/body-helper.js
@@ -25,7 +25,7 @@ AFRAME.registerComponent("body-helper", {
       oneOf: ACTIVATION_STATES
     },
     type: { default: "dynamic", oneOf: [TYPE.STATIC, TYPE.DYNAMIC, TYPE.KINEMATIC] },
-    emitCollisionEvents: { default: false },
+    emitCollisionEvents: { default: true },
     disableCollision: { default: false },
     collisionFilterGroup: { default: 1 }, //32-bit mask,
     collisionFilterMask: { default: 1 }, //32-bit mask
