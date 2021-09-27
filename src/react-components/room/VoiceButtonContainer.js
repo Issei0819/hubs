dfadd749d52a0a938f64a08d164e1e137f7bedfb
Issei0819 <issei.kurata819@gmail.com>
2021-09-27 12:11:08 +0900

    2021-09-27-ver09

--- src/react-components/room/VoiceButtonContainer.js
+++ src/react-components/room/VoiceButtonContainer.js
@@ -7,7 +7,7 @@ import { useMicrophone } from "./useMicrophone";
 import { FormattedMessage } from "react-intl";
 
 
-//talk_count: 会話回数,talk_time: 会話時間
+//talk_count: 会話回数,talk_time: 会話時間,is_talk: 会話識別
 var is_talk = false;
 var talk_count = 0;
 var talk_time = 0;
