06c82ec456d5445ca3569adcec596c8fe7e3b1cb
Issei0819 <issei.kurata819@gmail.com>
2021-10-05 05:11:47 +0900

    2021-10-05-ver03

--- src/network-schemas.js
+++ src/network-schemas.js
@@ -27,9 +27,18 @@ function registerNetworkSchemas() {
   NAF.schemas.add({
     template: '#my-template',
     components: [
-      'position',
-      'rotation',
-      'scale'
+      {
+        component: "position",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.001)
+      },
+      {
+        component: "rotation",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.5)
+      },
+      {
+        component: "scale",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.001)
+      }
     ]
   });
 
