427609ba894fd8fe414b1de9e59764ee0b7242ae
Issei0819 <issei.kurata819@gmail.com>
2021-11-20 01:06:51 +0900

    2021-11-20-ver05

--- src/components/tools/pen.js
+++ src/components/tools/pen.js
@@ -417,7 +417,7 @@ AFRAME.registerComponent("pen", {
       ) {
         this._getNormal(this.normal, this.worldPosition, this.direction);
         this.currentDrawing.draw(this.worldPosition, this.direction, this.normal, this.data.color, this.data.radius);
-        console.log(intersection.object.a-box)
+        console.log(intersection.object)
         console.log(intersection.object.el) //Change points
         //console.log(intersection.object.ws.el)
         //App.MessageDispatch.dispatch("Hit!!");
